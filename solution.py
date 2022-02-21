25ec6bbcb3ad2f435e36df83cdfeef92965060d9
mr6439 <98670531+mr6439@users.noreply.github.com>
2022-02-21 14:41:10 -0500

    Updated

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 from socket import *
 import base64
 
-msg = "\r\n I love Computer Networks"
+msg = "\r\n My message"
 endmsg = "\r\n.\r\n"
 
 mailserver = ("mail.smtp2go.com", 2525) #Fill in start #Fill in end
@@ -24,21 +24,9 @@ if recv1[:3] != '250':
     print('250 reply not received from server.')
 
 
-#Info for username and password
-username =  "abc@example.com"                     #the username for your server
-password = "pswd"                                    #the password for your server, changed here
-base64_str = ("\x00"+username+"\x00"+password).encode()
-base64_str = base64.b64encode(base64_str)
-authMsg = "AUTH PLAIN ".encode()+base64_str+"\r\n".encode()
-clientSocket.send(authMsg)
-recv_auth = clientSocket.recv(1024)
-print(recv_auth.decode())
-if recv1[:3] != '250':
-    print('250 reply not received from server.')
-
 
 # Send MAIL FROM command and print server response.
-mailFrom = "MAIL FROM: <anyemailid@gmail.com> \r\n"
+mailFrom = "MAIL FROM: <mohan@nyu.edu> \r\n"
 clientSocket.send(mailFrom.encode())
 recv2 = clientSocket.recv(1024)
 print("After MAIL FROM command: "+recv2)
@@ -46,7 +34,7 @@ if recv1[:3] != '250':
     print('250 reply not received from server.')
 
 # Send RCPT TO command and print server response.
-rcptTo = "RCPT TO: <destination@gmail.com> \r\n"
+rcptTo = "RCPT TO: <mohanraghu@gmail.com> \r\n"
 clientSocket.send(rcptTo.encode())
 recv3 = clientSocket.recv(1024)
 print("After RCPT TO command: "+recv3)
@@ -64,7 +52,7 @@ if recv1[:3] != '250':
 # Send message data.
 subject = "Subject: SMTP mail client testing \r\n\r\n" 
 clientSocket.send(subject.encode())
-message = raw_input("Enter your message: \r\n")
+message = "Data"
 clientSocket.send(message.encode())
 clientSocket.send(endmsg.encode())
 recv_msg = clientSocket.recv(1024)
